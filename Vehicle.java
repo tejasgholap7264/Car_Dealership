e114ad32e12836cf73d2bace6764d9ae6e9713d7
tejasgholap7264 <118683588+tejasgholap7264@users.noreply.github.com>
2024-05-22 23:27:34 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -3,7 +3,7 @@ package car_dealership;
 public class Vehicle {
     private String make;
     private String model;
-    private double price;//noone will able to access these variables directly outside of this class.
+    private double price;
 	
     public String getMake() {
 		return make;
@@ -26,6 +26,5 @@ public class Vehicle {
 	@Override
 	public String toString() {
 		return "Vehicle [make=" + make + ", model=" + model + ", price=" + price + "]";
-	}//to print the object(vehicle) when finance=false we will use toString method or else it will give output hashcode
-	
+	}
 }
